df89673b7e4438f061eeca126d6359790f511f58
code-shoot <franksmithattheshoot@gmail.com>
2020-12-23 10:46:36 +0530

    impoving home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
+import {ReactComponent as IntroImage } from './assets/intro-image.svg'
 import NavBar from './component/navbar/navbar.component'
 
 function App() {
@@ -9,16 +10,23 @@ function App() {
       <NavBar className='nav-bar'/>
     
       <div className="Home">
-        <div className="intro">
-          <h1>Hey!!! there</h1>
-          <p>
-            I am <span>Aditya Sharma</span>, 
+
+        <div className="intro-container">
+          <p className="intro">
+            <span className="hello-line">Hello!!! I'm</span> <br />
+            <span className="name">Aditya Sharma,</span><br /> 
             A Web Developer
           </p>
+          <div className="button-container">
+            <button className="say-hello-btn">SAY HELLO</button>
+            <button className="about-me-btn">ABOUT ME</button>   
+          </div>
         </div>
-        <div className="intro-image">
-          
+
+        <div className="intro-image-container">
+          <IntroImage className="intro-image"/>
         </div>
+
       </div>
     </div>
   );
